0bea8e8e375d773aab7b7afe7e00ef16620596e2
Kammradt <vinicius.kammradt1@gmail.com>
2021-11-28 22:13:57 -0300

    Adds tweet integration

--- src/dags/dag-random-card.py
+++ src/dags/dag-random-card.py
@@ -5,8 +5,11 @@ from dataclasses import dataclass, asdict
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.models import Variable
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
+import tweepy
+import os
 
 
 @dataclass
@@ -33,10 +36,35 @@ def print_card_info(card: Card):
     print(f'👇 You can download the image here: {card.url}')
 
 
+def get_authenticated_client() -> tweepy.API:
+    api_key = Variable.get('api_key')
+    api_key_secret = Variable.get('api_key_secret')
+    access_token = Variable.get('access_token')
+    access_token_secret = Variable.get('access_token_secret')
+    
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    return tweepy.API(auth)
+
+def tweet_card(card: Card):
+    client = get_authenticated_client()
+    filename = 'card_image.png'
+    
+    with open(filename, 'wb') as handler:
+        # Baixa a imagem
+        handler.write(get(card.url, allow_redirects=True).content)
+        #Faz o tweet
+        tweet_text = f'A carta aleatória de agora é: {card.name}, com valor de ${card.price}'
+        client.update_with_media(filename, tweet_text)
+        #Exclui o arquivo da imagem
+        os.remove(filename)
+
+
 with DAG(
     dag_id='random_card_dag',
     default_args=dict(retries=3, start_date=days_ago(7)),
-    schedule_interval=None,
+    schedule_interval='0 * * * *',
     catchup=False
 ) as dag:
     start = DummyOperator(task_id='Start')
@@ -60,6 +88,13 @@ with DAG(
         params=asdict(card),
     )
 
+    tweet = PythonOperator(
+        task_id='tweet',
+        python_callable=tweet_card,
+        op_args=[card]
+    )
+
+
     end = DummyOperator(task_id='End')
 
-    start >> print_card_info >> create_table_cards >> insert_card >> end
+    start >> print_card_info >> create_table_cards >> insert_card >> tweet >>end
